51c6c8fc76a80b68dda681fbb75302b366d4b7a6
Gabi App <gabi@almasense.com>
2026-02-14 20:11:11 -0300

    fix(metro): remove blockList pattern that broke EAS worker paths

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,19 +1,3 @@
 ﻿const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
-const config = getDefaultConfig(__dirname);
-
-// 🔒 TRAVA o Metro APENAS no mobile
-config.watchFolders = [
-  path.resolve(__dirname),
-];
-
-config.resolver.blockList = [
-  // Bloqueia tudo fora do mobile
-  /server\/.*/,
-  /drizzle\/.*/,
-  /shared\/.*/,
-  /expo\/.*/,
-];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
